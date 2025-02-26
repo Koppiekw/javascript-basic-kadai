84fb4b7ffd7ae4f2f43d1e4cd2d943f37655e935
Koppiekw <saki-oo@hotmail.co.jp>
2025-02-26 20:47:24 +0900

    kadai_mdn_jsをアップロード
    kadai_mdn_jsをアップロードしました。

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+
+const moonLanding = new Date;
+
+const month = (moonLanding.getMonth() + 1);
+console.log(moonLanding.getFullYear() + '年' +
+            month + '月' +
+            moonLanding.getDate() + '日');
+
